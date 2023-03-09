9273d4ccdae3f28f7bcc48d1c621e386a5d8a118
brijesh24bs <brijesh24.bs@gmail.com>
2023-03-09 09:37:03 +0530

    project structure, exception handling, and logger

--- setup.py
+++ setup.py
@@ -17,11 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='mlProject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Brijesh',
+author_email='brijesh24.bs@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
-
 )
